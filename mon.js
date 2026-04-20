52d36bed58ac83a86a8b2a37210ea1e4ac92a41f
danjuma benedict <ayubabenedict50@gmail.com>
2026-04-20 15:35:26 +0100

    added new files

---
+++ mon.js
@@ -0,0 +1,162 @@
+console.log("Hi" + " there");
+
+console.log("i am exited to talk to you");
+
+
+
+let bot;
+    bot = "teacher Bot";
+
+
+    let botLocation = "the Universe";
+
+    console.log("Allow me to introduce myself");
+
+
+    let botIntroduction = "My name is";
+
+    let intro = botIntroduction + " " + bot +  ".";
+    console.log(intro);
+
+let botLocationSentence = "I live in";
+
+let loca = botLocationSentence + " " + botLocation + ".";
+console.log(loca);
+
+bot = "professor Bot";
+
+    let nicknameIntroduction = "My nickname is ";
+
+        let nickname = nicknameIntroduction + bot + ".";
+
+console.log(nickname);
+
+bot = "awesomeTeacherBot";
+
+let newNickNameGreeting = "I love my nickname but i wish people would call me ";
+
+let steeze = newNickNameGreeting + bot + ".";
+
+console.log(steeze);
+
+let favouriteSubject = "Chemistry";
+
+let favouriteSubjectSentence = "My favourite Subject is ";
+
+let favour = favouriteSubjectSentence + favouriteSubject + ".";
+
+console.log(favour);
+
+console.log("Well it was nice talking to you, have a nice day!");
+
+let goodbye = "Goodbye 👋";
+let goodbyeSentence = "I hope to talk to you again soon, " ;
+
+let goodbyeMessage = `${goodbyeSentence} ${goodbye}.`;
+
+console.log(goodbyeMessage);
+
+console.log(typeof goodbye);
+
+//    monday's class on js 
+     let age = 20 ; 
+     if (age >= 18){
+    console.log('you can vote');
+     }
+     else{
+        console.log('you cannot vote');
+     }
+
+     let a = 6;
+     let b= 9;
+     let c= 6;
+     console.log(a >=b)
+    console.log(b >=a)
+    console.log(a >=c)
+    let myName ;
+    console.log(myName);
+    let name =null;
+    console.log(name);
+         
+    console.log(5 == "5");
+    console.log(5 === "5");
+    let ab = 5 + "5";
+    console.log(ab);
+     let ac = ("abcfh"=== "abcfh");
+     console.log(ac);
+ 
+     let myAge = 20;
+     let hasID = true;
+     console.log(myAge >= 18 && hasID);
+     console.log(myAge >= 21 || hasID);
+
+       let agee = 17;
+     if(agee >= 18){
+        console.log("you are welcome")
+     }
+
+      else if (agee >= 17){
+        console.log('you are luck')
+        }
+        else {
+            console.log("get out")
+        }
+
+        let day = 3;
+        switch(day){
+            case 1:
+                console.log('its monday');
+                 break;
+                 case 2:
+                console.log('its tue');
+                break;
+                 case 3:
+                console.log('its wed');
+                break;
+                 case 4:
+                console.log('its thurs');
+                
+        }
+        // console.log(542852841)
+
+        let creditScore = 720; 
+let annualIncome = 60000; 
+let loanAmount = 200000; 
+
+let eligibilityStatus;
+
+if (creditScore >= 750 && annualIncome >= 80000) {
+    eligibilityStatus = "Eligible for premium loan rates.";
+} else if (creditScore >= 700 && annualIncome >= 50000) {
+    eligibilityStatus = "Eligible for standard loan rates.";
+} else if (creditScore >= 650 && annualIncome >= 40000) {
+    eligibilityStatus = "Eligible for subprime loan rates.";
+} else if (creditScore < 650) {
+    eligibilityStatus = "Not eligible due to low credit score.";
+} else {
+    eligibilityStatus = "Not eligible due to insufficient income.";
+}
+
+console.log(eligibilityStatus);
+
+let score = 80;
+if (score >= 70){
+    console.log("A")
+}
+
+else if (score >= 65 ){
+    console.log("B")
+}
+else if (score >=45) {
+    console.log("c")
+}
+     
+else if (score >=25) {
+    console.log("d")
+}
+else if (score >=20) {
+    console.log("c")
+}
+else {
+    console.log("F")
+}
